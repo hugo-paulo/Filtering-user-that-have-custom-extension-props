b203af5c8144c26a3d9e03d7c7addd4c00dc588f
hugo-paulo <47899992+hugo-paulo@users.noreply.github.com>
2023-01-06 17:43:29 +0000

    Update Filtering users that have custom extension props.ps1

--- Filtering users that have custom extension props.ps1
+++ Filtering users that have custom extension props.ps1
@@ -1,13 +1,29 @@
-﻿ Connect-MgGraph
+Connect-MgGraph
 
- #This is the base line 
+#This is the base line 
+#? filter no being applied on the request
+$users = Invoke-MgGraphRequest -Uri "https://graph.microsoft.com/beta/users/?`$select=displayName,mail,id,createdDateTime,extension_a74d0a2daede4b4f847b5e0eb438b9f8_MyExtExample&`$filter=createdDateTime le 2022-12-02T16:00:00Z"
 
- $users = Invoke-MgGraphRequest -Uri "https://graph.microsoft.com/beta/users/?`$select=mail,id,createdDateTime,extension_a74d0a2daede4b4f847b5e0eb438b9f8_MyExtExample&`$filter=createdDateTime le 2022-12-02T16:00:00Z"
-
- $users.value 
+$users.value 
  
- #Remove all user that don't have extenison props
- $usersWithExt = $users.value | Where-Object {$_.extension_a74d0a2daede4b4f847b5e0eb438b9f8_MyExtExample -notcontains $null}
+#Remove all user that don't have extenison props
+$usersWithExt = $users.value | Where-Object {$_.extension_a74d0a2daede4b4f847b5e0eb438b9f8_MyExtExample -notcontains $null}
 
- #only return user with extension not equal to true 
- $usersWithExtNotTrue = $usersWithExt | Where-Object {$_.extension_a74d0a2daede4b4f847b5e0eb438b9f8_MyExtExample -notcontains "true"}
+#only return user with extension not equal to true 
+$usersWithExtNotTrue = $usersWithExt | Where-Object {$_.extension_a74d0a2daede4b4f847b5e0eb438b9f8_MyExtExample -notcontains "true"}
+
+$usersWithExtNotTrue
+
+
+#using cmdlt get user, might solve issue with pagination as above
+
+Select-MgProfile -Name "beta" #"1.0"
+
+
+$usersWithExt = Get-MgUser -all -Filter "createdDateTime le 2022-12-02T16:00:00Z" | Where-Object {$_.additionalProperties.extension_a74d0a2daede4b4f847b5e0eb438b9f8_MyExtExample -notcontains $null}
+
+
+$usersWithExtNotTrue = $usersWithExt | Where-Object {$_.additionalProperties.extension_a74d0a2daede4b4f847b5e0eb438b9f8_MyExtExample -notcontains "true"}
+
+$usersWithExtNotTrue 
+
